b49f128d3b66c2cb570bcfb7e435a599948619ae
Fawn <sarahmargaret@ymail.com>
2023-05-14 20:26:28 -0400

    added licenses

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-// TODO: Include packages needed for this application
+// PACKAGES
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
+
+
+// ARRAYS
 const questions = [
     {
         type: "input",
         name: "project_title",
-        message: "What is the name of your project? ",
+        message: "What is the name of the project?",
       },
       {
         type: "input",
         name: "project_description",
-        message: "",
+        message: "Please write a description of the project:",
       },
       {
         type: "input",
@@ -22,32 +24,58 @@ const questions = [
       {
         type: "input",
         name: "project_installation",
-        message: "",
+        message: "Are there any installation instructions:",
       },
       {
         type: "input",
         name: "project_usage",
-        message: "",
+        message: "Are there any directions for usage: ",
       },
       {
         type: "input",
         name: "project_license",
-        message: "",
+        message: "Please choose a license for the project: ",
+        choices: [
+            "None",
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Clause Simplified License",
+            "BSD 3-Clause New or Revised License",
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU General Public License v2.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense",
+        ]
       },
       {
         type: "input",
         name: "project_contributing",
-        message: "",
+        message: "Are there any contributing guidelines: ",
       },
       {
         type: "input",
         name: "project_tests",
-        message: "",
+        message: "Are there any instuctions for testing: ",
       },
       {
         type: "input",
         name: "project_questions",
         message: "",
+      },
+      {
+        type: "input",
+        name: "project_username",
+        message: "What is your GitHub username: ",
+      },
+      {
+        type: "input",
+        name: "project_email",
+        message: "What is your e-mail address: ",
       },
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  licenseList = [
+    {
+      license: "None",
+      label: "",
+      badge: "",
+      link: "",
+    }
+  ]
+}
+
+// "None",
+//             "Apache License 2.0",
+//             "GNU General Public License v3.0",
+//             "MIT License",
+//             "BSD 2-Clause Simplified License",
+//             "BSD 3-Clause New or Revised License",
+//             "Boost Software License 1.0",
+//             "Creative Commons Zero v1.0 Universal",
+//             "Eclipse Public License 2.0",
+//             "GNU Affero General Public License v3.0",
+//             "GNU General Public License v2.0",
+//             "GNU Lesser General Public License v2.1",
+//             "Mozilla Public License 2.0",
+//             "The Unlicense",
+
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
