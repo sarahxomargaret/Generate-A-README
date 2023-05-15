02db69f33fca83b4f71d7db6b87da21cdc8a9b96
Fawn <sarahmargaret@ymail.com>
2023-05-14 20:00:22 -0400

    added title, description, table of contents, installation, usage, license, contributing, tests, and questions

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "project_title",
+        message: "What is the name of your project? ",
+      },
+      {
+        type: "input",
+        name: "project_description",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_tableOfContents",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_installation",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_usage",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_license",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_contributing",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_tests",
+        message: "",
+      },
+      {
+        type: "input",
+        name: "project_questions",
+        message: "",
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
