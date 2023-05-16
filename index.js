31a4f36311fb5445b63553318b3c28c530a488db
Fawn <sarahmargaret@ymail.com>
2023-05-16 17:17:46 -0400

    added function call

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const questions = [
         message: "Are there any directions for usage: ",
       },
       {
-        type: "input",
+        type: "list",
         name: "project_license",
         message: "Please choose a license for the project: ",
         choices: [
@@ -41,15 +41,7 @@ const questions = [
             "GNU General Public License v3.0",
             "MIT License",
             "BSD 2-Clause Simplified License",
-            "BSD 3-Clause New or Revised License",
-            "Boost Software License 1.0",
-            "Creative Commons Zero v1.0 Universal",
-            "Eclipse Public License 2.0",
-            "GNU Affero General Public License v3.0",
-            "GNU General Public License v2.0",
-            "GNU Lesser General Public License v2.1",
             "Mozilla Public License 2.0",
-            "The Unlicense",
         ]
       },
       {
@@ -80,10 +72,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((data) => ("generateREADME", data));
+}
 // Function call to initialize app
 init();
