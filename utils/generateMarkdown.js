4528f24622a68b45f39cea4a405f6cfcdf12aa11
Fawn <sarahmargaret@ymail.com>
2023-05-16 17:29:30 -0400

    added licenselist

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,23 +8,38 @@ function renderLicenseBadge(license) {
       badge: "",
       link: "",
     }
+    {
+      license: "Apache License 2.0",
+      label: "License",
+      badge: "",
+      link: "",
+    }
+    {
+      license: "GNU General Public License v3.0",
+      label: "License",
+      badge: "",
+      link: "",
+    }
+    {
+      license: "MIT License",
+      label: "License",
+      badge: "",
+      link: "",
+    }
+    {
+      license: "BSD 2-Clause Simplified License",
+      label: "License",
+      badge: "",
+      link: "",
+    }
+    {
+      license: "Mozilla Public License 2.0",
+      label: "License",
+      badge: "",
+      link: "",
+    }
   ]
 }
-
-// "None",
-//             "Apache License 2.0",
-//             "GNU General Public License v3.0",
-//             "MIT License",
-//             "BSD 2-Clause Simplified License",
-//             "BSD 3-Clause New or Revised License",
-//             "Boost Software License 1.0",
-//             "Creative Commons Zero v1.0 Universal",
-//             "Eclipse Public License 2.0",
-//             "GNU Affero General Public License v3.0",
-//             "GNU General Public License v2.0",
-//             "GNU Lesser General Public License v2.1",
-//             "Mozilla Public License 2.0",
-//             "The Unlicense",
 
 
 
